40de6fbba330cdfad5f1297e43be496d0c691646
Yusef Noor <ynr2321@gmail.com>
2026-02-15 01:05:04 +0000

    added postgres container to the app in Aspire Apphost

--- backend/AspireApp/AspireApp.AppHost/Program.cs
+++ backend/AspireApp/AspireApp.AppHost/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice");
@@ -6,6 +7,10 @@ var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
 builder.AddNpmApp("frontend", "../../../frontend", "dev")
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+// Add postgreSQL database
+builder.AddPostgres("postgres")
     .WithReference(apiService);
 
 builder.Build().Run();
