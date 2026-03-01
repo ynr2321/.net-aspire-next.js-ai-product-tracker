2f8d547ca2b349890adcb8cef04117d41d44f6db
ynr2321 <ynr2321@gmail.com>
2026-03-01 01:15:49 +0000

    removed other nextly related boiler plate adn added link to linkedin adn github

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -24,14 +24,14 @@ export const Navbar = () => {
           <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
                 <Image
-                  src="/img/logo.svg"
+                  src="/img/logo.png"
                   width="32"
-                  alt="N"
+                  alt="ProductTracker"
                   height="32"
                   className="w-8"
                 />
               </span>
-            <span>Nextly</span>
+            <span>ProductTracker</span>
           </span>
         </Link>
 
